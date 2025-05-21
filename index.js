9eb41a8eb1def1624026e9569bd66ac91cc08ee6
RifatRahman7 <rifatdcian@gmail.com>
2025-05-22 02:06:04 +0600

    Added env file

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-//user: freelanceMartDB
-//pass: 7mNYpTaIJiKr7Ixq
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gle9ffl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,7 +27,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
