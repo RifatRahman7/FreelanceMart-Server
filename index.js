69f0b4d8fa037e2d1bc0151b26ce78c2fb616abe
RifatRahman7 <rifatdcian@gmail.com>
2025-06-02 02:17:40 +0600

    Added posted task

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+const { ObjectId } = require('mongodb');
 require('dotenv').config()
 app.use(cors());
 app.use(express.json());
@@ -25,19 +26,43 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const tasksCollection = client.db("freelanceMart").collection("tasks");
-    app.get("/tasks", async (req, res) => {
-      const result = await tasksCollection.find().sort({deadline:1}).toArray();
-      console.log(result);
+    app.get("/my-tasks", async (req, res) => {
+      const userEmail = req.query.email;
+      //console.log(userEmail);
+      if (!userEmail) {
+        return res.status(400).send("Email query parameter is required");
+      }
+      const result = await tasksCollection.find({userEmail}).toArray();
       res.send(result);
     })
+    app.get("/tasks", async (req, res) => {
+      const result = await tasksCollection.find().sort({deadline:1}).toArray();
+       //console.log(result);
+      res.send(result);
+    })
+
+     
+
     app.get("/tasks/featured", async (req, res) => {
       const result = await tasksCollection.find().sort({deadline:1}).limit(6).toArray();
       console.log(result);
       res.send(result);
     });
+    app.get('/tasks/:id', async (req, res) => {
+      const { id } = req.params;
+      try {
+        const task = await taskCollection.findOne({ _id: new ObjectId(id) });
+        if (!task) {
+          return res.status(404).send({ message: "Task not found" });
+        }
+        res.send(task);
+      } catch (error) {
+        res.status(500).send({ message: "Invalid task ID", error });
+      }
+    });
     app.post("/tasks", async (req, res) => {
       const task = req.body;
-      console.log(task);
+      // console.log(task);
       const result = await tasksCollection.insertOne(task);
       res.send(result);
     });
